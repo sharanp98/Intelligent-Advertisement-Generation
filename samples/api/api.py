78ece6c487fc6599a62500931209239c80a76442
Sharan <sharanp98@gmail.com>
2020-05-24 12:23:46 +0530

    fix bg, change font

--- samples/api/api.py
+++ samples/api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for,render_template, request, send_from_directory,send_file
+from flask import Flask, redirect, url_for, render_template, request, send_from_directory, send_file
 import os
 import json
 import re
@@ -7,17 +7,20 @@ from werkzeug import secure_filename
 app = Flask(__name__)
 
 
-WORKING_FOLDER = os.path.dirname(os.getcwd()) #samples
-UPLOAD_FOLDER =  WORKING_FOLDER
-DOWNLOAD_FOLDER = os.path.join(WORKING_FOLDER,'api/static')                                 
+WORKING_FOLDER = os.path.dirname(os.getcwd())  # samples
+UPLOAD_FOLDER = WORKING_FOLDER
+DOWNLOAD_FOLDER = os.path.join(WORKING_FOLDER, 'api/static')
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/no-need', methods=['GET', 'POST'])
 def upload_file():
@@ -37,57 +40,60 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'sample.jpg'))
             print('Image Uploaded')
             return redirect('/final')
-    return render_template("upload_image.html") 
+    return render_template("upload_image.html")
+
 
 @app.route('/final')
 def hello_admin():
 
     print('In final')
     os.chdir(WORKING_FOLDER)
-    #Delete previous output
+    # Delete previous output
     for file in os.listdir():
-        if file in ['labels.txt','reqd_images.txt','reqd_stickers.txt']:
+        if file in ['labels.txt', 'reqd_images.txt', 'reqd_stickers.txt']:
             os.remove(file)
-   
-    
+
     for file in os.listdir('segmented_images'):
         if file != 'background.jpeg':
-            os.remove(os.path.join('segmented_images',file))
+            os.remove(os.path.join('segmented_images', file))
     os.system("python3 pt1.py")
     data = open('labels.txt').read().splitlines()
-    return render_template("ListCategories.html",data=json.dumps(data))
+    return render_template("ListCategories.html", data=json.dumps(data))
 
-@app.route('/reqjson',methods=['POST'])
+
+@app.route('/reqjson', methods=['POST'])
 def pass_val():
-    name=request.form['canvas_data']
+    name = request.form['canvas_data']
     print(name)
     os.chdir(WORKING_FOLDER)
-    f = open("reqd_images.txt","w")
+    f = open("reqd_images.txt", "w")
     x = re.findall(r'[\w\d%!_ ]+', name)
     for i in x:
         f.write(i)
-        f.write("\n")                                                          
+        f.write("\n")
     f.close()
     os.system("python3 pt2.py")
     try:
-        os.remove("api/static/final.png")  
+        os.remove("api/static/final.png")
     except:
-        print("final.png does not exist")  
-    os.system("python3 copyimage.py") 
+        print("final.png does not exist")
+    os.system("python3 copyimage.py")
     return redirect('/disp')
+
 
 @app.route('/disp')
 def disp():
     return render_template("displayimage.html")
 
 
-@app.route('/download',methods=['GET', 'POST'])
+@app.route('/download', methods=['GET', 'POST'])
 def download():
-    return send_from_directory(directory= DOWNLOAD_FOLDER, filename='final.png')
+    return send_from_directory(directory=DOWNLOAD_FOLDER, filename='final.png')
 
-@app.route('/',methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def demo():
-    
+
     os.chdir(DOWNLOAD_FOLDER)
     for file in os.listdir():
         if file in ['final.png']:
@@ -108,51 +114,56 @@ def demo():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'sample.jpg'))
             print('Image Uploaded')
             return redirect('/background')
-    return render_template("upload2.html") 
+    return render_template("upload2.html")
 
-@app.route('/sticker',methods=['GET', 'POST'])
+
+@app.route('/sticker', methods=['GET', 'POST'])
 def sticker():
     return render_template('stickers.html')
 
-@app.route('/demo',methods=['GET', 'POST'])
+
+@app.route('/demo', methods=['GET', 'POST'])
 def demo1():
     return render_template('stickdemo.html')
 
 
-@app.route('/reqSticker',methods=['POST'])
+@app.route('/reqSticker', methods=['POST'])
 def reqSticker():
     name = request.form.get('canvas_data')
     print(name)
-    f = open("reqd_stickers.txt","w")
+    f = open("reqd_stickers.txt", "w")
     x = re.findall(r'[\d]+', name)
     for i in x:
         f.write(i)
-        f.write("\n")                                                        
+        f.write("\n")
     f.close()
     os.chdir(WORKING_FOLDER)
-    os.system("python3 addsticker.py") 
+    os.system("python3 addsticker.py")
 
 
 @app.route('/dispSticker')
 def dispSticker():
     return render_template("displaysticker.html")
 
-@app.route('/background',methods=['GET', 'POST'])
+
+@app.route('/background', methods=['GET', 'POST'])
 def backgrounds():
     return render_template('backgrounds.html')
 
-@app.route('/reqBackground',methods=['POST'])
+
+@app.route('/reqBackground', methods=['POST'])
 def reqBackground():
     name = request.form.get('canvas_data')
     # print("Background name received at server is: "+name[0])
     x = re.findall(r'[\d]+', name)
     print("Background name received at server is: "+x[0])
-    print("rewd_backgrounds.txt is saved at: "+os.getcwd())
-    with open("reqd_backgrounds.txt","w") as f:
+    os.chdir(os.path.join(WORKING_FOLDER, 'api'))
+    print("reqd_backgrounds.txt is saved at: "+os.getcwd())
+    with open("reqd_backgrounds.txt", "w") as f:
         f.write(str(x[0]))
-        f.write("\n")                                                        
+        f.write("\n")
     os.chdir(WORKING_FOLDER)
-    os.system("python3 addbackground.py") 
+    os.system("python3 addbackground.py")
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
--- samples/pt2.py
+++ samples/pt2.py
@@ -1,7 +1,7 @@
 import os
-from PIL import Image, ImageChops,ImageDraw,ImageFont
+from PIL import Image, ImageChops, ImageDraw, ImageFont
 import numpy as np
-import glob 
+import glob
 
 with open(os.getcwd()+'/reqd_images.txt', 'r') as f:
     reqd = f.read().splitlines()
@@ -13,14 +13,18 @@ for file in os.listdir('./segmented_images'):
             os.remove('./segmented_images/'+file)
 
 os.chdir('./segmented_images')
+
+
 def convert_bg(file):
-    img=Image.open(file).convert('RGBA')
-    arr=np.array(np.asarray(img))
-    r,g,b,a=np.rollaxis(arr,axis=-1)
-    mask=((r==255)&(g==255)&(b==255))
-    arr[mask,3]=0
-    img=Image.fromarray(arr,mode='RGBA')
+    img = Image.open(file).convert('RGBA')
+    arr = np.array(np.asarray(img))
+    r, g, b, a = np.rollaxis(arr, axis=-1)
+    mask = ((r == 255) & (g == 255) & (b == 255))
+    arr[mask, 3] = 0
+    img = Image.fromarray(arr, mode='RGBA')
     img.save('./'+file)
+
+
 for file in glob.glob("*.png"):
     convert_bg(file)
 
@@ -29,16 +33,19 @@ for img in glob.glob('*.png'):
     temp = img.split('_')[0]
     imgs.append(img)
 
+
 def trim(im):
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
     diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff,2.0, -100)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
+
+
 for img in imgs:
     temp = img
-    bg = Image.open(img) 
+    bg = Image.open(img)
     new_im = trim(bg)
     new_im.save(temp)
 
@@ -48,25 +55,26 @@ total_width = sum(widths) + (len(imgs)*20)
 max_height = max(heights)+80
 new_im = Image.new('RGB', (total_width, max_height))
 x_offset = 20
-for i,im in enumerate(images):
+for i, im in enumerate(images):
     y_offset = max_height//2 - heights[i]//2
-    new_im.paste(im, (x_offset,y_offset),im)
+    new_im.paste(im, (x_offset, y_offset), im)
     x_offset += im.size[0]+20
 new_im.save('./merged_black.png')
-img=Image.open('./merged_black.png').convert('RGBA')
-arr=np.array(np.asarray(img))
-r,g,b,a=np.rollaxis(arr,axis=-1)
-mask=((r==0)&(g==0)&(b==0))
-arr[mask,3]=0
-img=Image.fromarray(arr,mode='RGBA')
+img = Image.open('./merged_black.png').convert('RGBA')
+arr = np.array(np.asarray(img))
+r, g, b, a = np.rollaxis(arr, axis=-1)
+mask = ((r == 0) & (g == 0) & (b == 0))
+arr[mask, 3] = 0
+img = Image.fromarray(arr, mode='RGBA')
 img.save('./merged_transparent.png')
 
 img = Image.open("merged_transparent.png")
 background = Image.open("background.jpg")
 size = img.size
-background = background.resize(size,Image.ANTIALIAS)
+background = background.resize(size, Image.ANTIALIAS)
 background.paste(img, (0, 0), img)
-background.save('merged_gradient.png',"PNG")
+background.save('merged_gradient.png', "PNG")
+
 
 def process_img(txt):
     image = Image.open('merged_gradient.png')
@@ -76,7 +84,7 @@ def process_img(txt):
 
     # portion of image width you want text width to be
     img_fraction = 0.50
-    font_path = "/usr/share/fonts/truetype/freefont/AmaticSC-Regular.ttf"
+    font_path = "/usr/share/fonts/truetype/freefont/Bison-Bold.ttf"
     font = ImageFont.truetype(font_path, fontsize)
     while font.getsize(txt)[0] < img_fraction*image.size[0]:
         # iterate until the text size is just larger than the criteria
@@ -88,8 +96,10 @@ def process_img(txt):
     font = ImageFont.truetype(font_path, fontsize)
     # text_w, text_h = w//2, h-h//20
 
-    print ('final font size',fontsize)
-    draw.text((10,25), txt, font=font) # put the text on the image
-    image.save('final.png') # save it
+    print('final font size', fontsize)
+    draw.text((10, 25), txt, font=font)  # put the text on the image
+    image.save('final.png')  # save it
+
+
 process_img(offer)
 print('Success!')
